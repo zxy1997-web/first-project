59d1b8f23c297fa6d4d6a7943f76bb2a188960e9
zxy1997-web <627345391@qq.com>
2019-12-21 10:08:20 +0800

    黑马头条PC-登录模块-登录成功及失败处理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,27 +1,31 @@
 <template>
   <div class="login">
-      <el-card class="login-card">
-          <div class="title">
-              <img src="../../assets/img/logo_index.png" alt="">
-          </div>
-          <!-- 放置表单 el-form model 绑定数据对象-->
-          <el-form :model="loginForm" :rules="loginRules" ref="formName">
-              <!-- 表单域里面放置input/select/checkbox -->
-              <el-form-item prop="moblie">
-                  <el-input v-model="loginForm.moblie" placeholder="请输入手机号"></el-input>
-              </el-form-item>
-              <el-form-item prop="code">
-                  <el-input v-model="loginForm.code" style="width:65%" placeholder="验证码"></el-input>
-                  <el-button style="float:right" plain>发送验证码</el-button>
-              </el-form-item>
-              <el-form-item prop="check">
-                  <el-checkbox v-model="loginForm.check">我已阅读并同意用户登录协议和隐私条款</el-checkbox>
-              </el-form-item>
-              <el-form-item>
-                  <el-button @click="submitLogin()" type="primary" style="width:100%">登录</el-button>
-              </el-form-item>
-          </el-form>
-      </el-card>
+    <!-- 放置一个el-card组件 -->
+    <el-card class='login-card'>
+      <!-- 放置标题图片 -->
+      <div class='title'>
+        <img src="../../assets/img/logo_index.png" alt="">
+      </div>
+      <!-- 放置表单  el-form model  绑定数据对象 -->
+      <el-form ref="myForm" :model="loginForm" :rules="loginRules">
+        <!-- 表单域 里面  prop要写要检验的字段名  放置 input/select/checkbox 相当于一行-->
+        <el-form-item prop="mobile">
+           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <!-- 表单域 -->
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:65%" placeholder="验证码"></el-input>
+            <el-button style="float:right" plain>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item prop="check">
+          <!-- 复选框 -->
+          <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="submitLogin" type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -30,16 +34,18 @@ export default {
   data () {
     return {
       loginForm: {
-        moblie: '',
-        code: '',
-        check: false
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 是否勾选 同意被坑
       },
       loginRules: {
-        //   验证规则对象 key(字段名):value(规则 => [])
-        moblie: [{ required: true, message: '请输入您的手机号' },
-          { pattern: /^1[3456789]\d{9}$/, message: '请输入正确的手机号码' }],
-        code: [{ required: true, message: '请输入您的验证码' },
-          { pattern: /^\d{6}$/, message: '请输入正确的验证码' }],
+        // 验证规则对象 key(字段名):value(规则 => [])
+        mobile: [{ required: true, message: '请输入您的手机号' }, {
+          pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确'
+        }],
+        code: [{ required: true, message: '请输入你的验证码' }, {
+          pattern: /^\d{6}$/, message: '验证码格式不正确'
+        }],
         check: [{ validator: function (rule, value, callback) {
           // 自定义校验函数
           // rule 规则 没啥用
@@ -47,22 +53,37 @@ export default {
           // callback 是一个回调函数
           if (value) {
             // 认为已经勾选
-            callback()// 认为当前的规则校验通过了
+            callback() // 认为当前的规则校验通过了
           } else {
             // 认为没有勾选
-            callback(new Error('您应该同意我们的霸王条款，让我们欺负你'))// 如果没有勾选 认为当前校验没有通过
+            callback(new Error('您应该同意我们的霸王条款,让我们欺负你')) // 如果没有勾选 认为当前校验失败 应该停止
           }
         } }]
+
       }
     }
   },
   methods: {
-    submitLogin (formName) {
+    submitLogin () {
       // 手动校验
-      this.$refs.formName.validate(function (isOK) {
+      this.$refs.myForm.validate((isOK) => {
         if (isOK) {
           // 说明校验通过 应该调用接口
-          alert('成功')
+          this.$axios({
+            url: '/authorizations', // 请求地址 axios 没有指定 类型 默认走get类型
+            method: 'post', // 类型
+            data: this.loginForm // body 参数
+          }).then(result => {
+            // 只接受正确结果
+            // 前端缓存 登录成功返回给我们的令牌
+            window.localStorage.setItem('user-token', result.data.data.token)
+            this.$router.push('/home') // 跳转到home页
+          }).catch(() => {
+            this.$message({
+              type: 'warning',
+              message: '手机号或者验证码错误!'
+            })
+          })
         }
       })
     }
