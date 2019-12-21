26ca87c0cb8ca20aad992771f07a3b1bd980f1da
zxy1997-web <627345391@qq.com>
2019-12-21 20:08:13 +0800

    黑马头条PC-主页模块-头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -7,12 +7,12 @@
       <el-col class="right" :span="12">
           <el-row type="flex" justify="end" align="middle">
               <img :src="userInfo.photo ? userInfo.photo : defaultImg" alt="">
-              <el-dropdown>
+              <el-dropdown @command="clickMenu">
                   <span>{{userInfo.name}}</span>
                   <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>个人信息</el-dropdown-item>
-                    <el-dropdown-item>git地址</el-dropdown-item>
-                    <el-dropdown-item>退出</el-dropdown-item>
+                    <el-dropdown-item command="info">个人信息</el-dropdown-item>
+                    <el-dropdown-item command="git">git地址</el-dropdown-item>
+                    <el-dropdown-item command="lgout">退出</el-dropdown-item>
                   </el-dropdown-menu>
               </el-dropdown>
           </el-row>
@@ -39,6 +39,18 @@ export default {
     }).then(result => {
       this.userInfo = result.data.data
     })
+  },
+  methods: {
+    clickMenu (command) {
+      if (command === 'info') {
+
+      } else if (command === 'git') {
+        window.location.href = 'https://github.com/zxy1997-web/first-project'
+      } else {
+        window.localStorage.removeItem('user-token')
+        this.$router.push('/login')
+      }
+    }
   }
 }
 </script>
