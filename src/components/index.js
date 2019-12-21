6e36d5f9d18831b83bf59269aabd85abd9958b94
zxy1997-web <627345391@qq.com>
2019-12-21 11:42:13 +0800

    黑马头条PC-统一全局注册插件的使用方式Vue.use

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import layoutAside from './home/layout-aside'
+import layoutHeader from './home/layout-header'
+export default {
+  install (Vue) {
+    Vue.component('layout-aside', layoutAside)
+    Vue.component('layout-header', layoutHeader)
+  }
+}
