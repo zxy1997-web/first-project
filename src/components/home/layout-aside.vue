dd9a9705478252f54af31cc98430c07460cf2204
zxy1997-web <627345391@qq.com>
2019-12-21 10:53:31 +0800

    黑马头条PC-主页模块-左侧导航菜单

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="layout-aside">
+    <div class="title">
+        <img src="../../assets/img/logo_admin.png" alt="">
+    </div>
+    <!-- 左侧导航 -->
+    <el-menu background-color="#323745" text-color="#adafb5" active-text-color=" #ffd04b">
+        <!-- 没有折叠 -->
+        <el-menu-item>首页</el-menu-item>
+        <el-submenu>
+            <!-- 具名插槽 -->
+            <template slot="title">
+                <span>内容管理</span>
+            </template>
+            <el-menu-item>发布文章</el-menu-item>
+            <el-menu-item>文章列表</el-menu-item>
+            <el-menu-item>评论管理</el-menu-item>
+            <el-menu-item>素材管理</el-menu-item>
+        </el-submenu>
+        <el-submenu>
+            <!-- 具名插槽 -->
+            <template slot="title">
+                <span>粉丝管理管理</span>
+            </template>
+            <el-menu-item>图文数据</el-menu-item>
+            <el-menu-item>粉丝概况</el-menu-item>
+            <el-menu-item>粉丝画像</el-menu-item>
+            <el-menu-item>粉丝列表</el-menu-item>
+        </el-submenu>
+        <el-menu-item>账户信息</el-menu-item>
+    </el-menu>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.layout-aside{
+    width: 230px;
+    .el-menu{
+        border-right: none;
+    };
+    .title{
+        background-color: #2e2f32;
+        text-align: center;
+        padding:10px 0;
+        img{
+            height:35px;
+        }
+    }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>主页</div>
+  <!-- 放置一个大容器 -->
+  <el-container>
+    <el-aside style="height:100vh;background-color:#323745;width:230px">
+     <layout-aside></layout-aside>
+    </el-aside>
+    <el-container>
+      <!-- 上下布局 -->
+      <el-header>toubu</el-header>
+      <el-main>zhuyao</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
