12db93695edcbd6c0ac8cf5b48f5ad8a6c8d6bec
zxy1997-web <627345391@qq.com>
2019-12-27 19:49:13 +0800

    面包屑组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
+import breadCrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }
