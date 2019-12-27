a1f6f1efe368c316261f8d93f34675c1dddf0860
zxy1997-web <627345391@qq.com>
2019-12-27 19:55:32 +0800

    新建页面挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,11 @@ const routes = [
     children: [{
       path: '',
       component: Home2
-    }]
+    }, {
+      path: 'comment',
+      component: () => import('../views/comment')
+    }
+    ]
   },
   {
     path: '/login',
