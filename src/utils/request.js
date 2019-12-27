6da2a6886d0a1206a55fd7db0549e05de6b17986
zxy1997-web <627345391@qq.com>
2019-12-27 10:11:30 +0800

    统一处理异常数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import router from '../router'
+import { Message } from 'element-ui'
 // 封装一个axios
 
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 设置一个常态值
@@ -20,6 +22,25 @@ axios.interceptors.response.use(function (response) {
   return response.data ? response.data : {}
 }, function (error) {
 // 失败时执行该函数
-  return Promise.reject(error)
+  let status = error.response.status
+  let message = ''
+  switch (status) {
+    case 400:
+      message = '请求参数失败'
+      break
+    case 507 :
+      message = '服务器数据库异常'
+      break
+    case 401:
+      window.localStorage.removeItem('user-token')// 删除过期的token
+      router.push('/login')
+      break
+    case 403:
+      message = '没有设置这条评论的权限'
+      break
+    default:
+      break
+  }
+  Message({ type: 'warning', message })
 })
 export default axios
