169fd3dca9dccc3785bcb1d72e8f76a28c4803a7
zxy1997-web <627345391@qq.com>
2019-12-29 17:08:08 +0800

    数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -6,9 +6,25 @@
       </template>
       </bread-crumb>
       <!-- 标签页 -->
-      <el-tabs v-model="activeName">
-          <el-tab-pane label="全部图片" name="all">全部</el-tab-pane>
-          <el-tab-pane label="收藏图片" name="collect">收藏</el-tab-pane>
+      <el-tabs v-model="activeName" @tab-click="changeTab">
+          <el-tab-pane label="全部图片" name="all">
+            <!-- 生成页面结构 -->
+            <div class="img-list">
+              <el-card class="img-card" v-for="item in list " :key="item.id">
+                <img :src="item.url" alt="">
+                <el-row class="opeate" type="flex" justify="space-around" align="middle">
+                  <i class="el-icon-star-on"></i><i class="el-icon-delete-solid"></i>
+                </el-row>
+              </el-card>
+            </div>
+            </el-tab-pane>
+          <el-tab-pane label="收藏图片" name="collect">
+            <div class="img-list">
+              <el-card class="img-card" v-for="item in list " :key="item.id">
+                <img :src="item.url" alt="">
+              </el-card>
+            </div>
+          </el-tab-pane>
       </el-tabs>
   </el-card>
 </template>
@@ -17,12 +33,54 @@
 export default {
   data () {
     return {
-      activeName: 'collect'
+      activeName: 'all',
+      list: []
     }
+  },
+  methods: {
+    // 切换页签方法
+    changeTab () {
+      this.getMaterial()
+    },
+    getMaterial () {
+      this.$axios({
+        url: '/user/images',
+        params: {
+          collect: this.activeName === 'collect'
+        }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getMaterial()
   }
 }
 </script>
 
-<style>
-
+<style lang='less' scpoped>
+.img-list{
+  display: flex;
+  flex-wrap: wrap;
+  .img-card{
+    width: 200px;
+    height: 220px;
+    margin: 20px 50px;
+    position: relative;
+    img{
+      width: 100%;
+      height: 100%;
+    }
+    .opeate{
+      width: 100%;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      font-size: 20px;
+      height: 36px;
+      background-color: #f4f5f6;
+    }
+  }
+}
 </style>
