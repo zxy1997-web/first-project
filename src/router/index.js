c87cc3304879bc645d4ba42c5931ab0344922760
zxy1997-web <627345391@qq.com>
2019-12-30 09:29:18 +0800

    文章列表-新建页面-挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,9 @@ const routes = [
     }, {
       path: 'material',
       component: () => import('../views/material')
+    }, {
+      path: 'articles',
+      component: () => import('../views/articles')
     }
     ]
   },
