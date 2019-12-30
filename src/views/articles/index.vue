d0d3c345c08cab7a6ff370804375a15cda4991da
zxy1997-web <627345391@qq.com>
2019-12-30 10:08:38 +0800

    页面结构，搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -3,12 +3,56 @@
       <bread-crumb slot="header">
       <template slot="title">文章列表</template>
       </bread-crumb>
+      <!-- 表单容器 -->
+      <el-form style="padding-left:50px">
+          <el-form-item label="文章状态">
+              <el-radio-group v-model="searchForm.status">
+                  <el-radio :label="5">全部</el-radio>
+                  <el-radio :label="0">草稿</el-radio>
+                  <el-radio :label="1">待审核</el-radio>
+                  <el-radio :label="2">审核通过</el-radio>
+                  <el-radio :label="3">审核失败</el-radio>
+              </el-radio-group>
+
+          </el-form-item>
+          <el-form-item label="频道列表">
+              <el-select placeholder="请选择频道" v-model="searchForm.channel_id">
+                  <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+              </el-select>
+          </el-form-item>
+          <el-form-item label="时间选择">
+              <el-date-picker v-model="searchForm.dateRange" type="daterange"></el-date-picker>
+          </el-form-item>
+
+      </el-form>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      searchForm: {
+        status: 5, // 默认值
+        channels_id: null,
+        dateRange: []
+      },
+      channels: [],
+      value: ''
+    }
+  },
+  methods: {
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
 }
 </script>
 
